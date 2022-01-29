217aacee27aad80d31782514ea6a25fe0ed209b4
Tiffany Stokley <tiffanystokley@tiffanys-mbp.lan>
2022-01-29 13:21:58 -0500

    updates

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,41 +1,105 @@
-const SHA256 = require('crypto-js/sha256')
+const SHA256 = require('crypto-js/sha256');
+const { DIFFICULTY, MINE_RATE} = require('../config')
 
 class Block {
-    constructor(timestamp, lastHash, hash, data){
+
+    /**
+     * @class An object representing a single Block to be added to a blockchain
+     * @param {*} timestamp 
+     * @param {*} lastHash 
+     * @param {*} hash 
+     * @param {*} data 
+     * @param {*} nonce 
+     * @param {*} difficulty 
+     */
+    constructor(timestamp, lastHash, hash, data, nonce, difficulty){
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data; 
+        this.nonce = nonce;
+        this.difficulty = difficulty || DIFFICULTY;
     }
 
+    /**
+     * String representation of a Block object
+     * @returns {String} 
+     */
     toString() {
         return `Block -
-            Timestamp: ${this.timestamp}
-            Last Hash: ${this.lastHash.substring(0,10)}
-            Hash     : ${this.hash.substring(0,10)}
-            Data     : ${this.data}
+            Timestamp : ${this.timestamp}
+            Last Hash : ${this.lastHash.substring(0,10)}
+            Hash      : ${this.hash.substring(0,10)}
+            Nonce     : ${this.nonce}
+            Difficulty: ${this.difficulty}
+            Data      : ${this.data}
         `
     }
 
+    /**
+     * Creates the first Block in the blockchain
+     * @returns {Block} the first block added to the blockchain
+     */
     static genesis() {
-        return new this('Genesis Time Stamp', '------', 'f1dsd-djs', '[]')
+        return new this('Genesis Time Stamp', '------', 'f1dsd-djs', '[]', 0, DIFFICULTY)
     }
 
+    /** 
+    * Add a block to the block chain using POW system inspired by HashCash
+    * @param {Block} lastBlock - The block to add to the blockchain
+    * @param {any} data - The data to be added to the block
+    * @returns {Block} Then newly added block
+    */
     static mineBlock(lastBlock, data){
-        const timestamp = Date.now();
+        let hash, timestamp;
         const lastHash = lastBlock.hash;
-        const hash = Block.hash(timestamp, lastHash, data);
-        return new this(timestamp, lastHash, hash, data)
+        let {difficulty} = lastBlock;
+        let nonce = 0;
 
+        do{
+            nonce++;
+            timestamp = Date.now();
+            difficulty = Block.adjustDifficulty(lastBlock, timestamp)
+            hash = Block.hash(timestamp, lastHash, data, nonce, difficulty);
+        } while(hash.substring(0, difficulty) != '0'.repeat(difficulty));
+
+        return new this(timestamp, lastHash, hash, data, nonce, difficulty)
     }
 
-    static hash(timestamp, lastHash, data){
-        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    /**
+     * 
+     * @param {*} timestamp 
+     * @param {*} lastHash 
+     * @param {*} data 
+     * @param {*} nonce 
+     * @param {*} difficulty
+     * @returns 
+     */
+    static hash(timestamp, lastHash, data, nonce, difficulty){
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
     }
 
+    /**
+     * 
+     * @param {Block} block 
+     * @returns 
+     */
     static blockHash(block){
-        const{timestamp, lastHash, data} = block;
-        return Block.hash(timestamp, lastHash, data);
+        const{timestamp, lastHash, data, nonce, difficulty} = block;
+        return Block.hash(timestamp, lastHash, data, nonce, difficulty);
+    }
+
+    /**
+     * Icrements or decrements the difficulty parameter depending on how quickly the
+     * last block was mined
+     * @param {Block} lastBlock 
+     * @param {*} currentTime 
+     * @returns {int} difficulty
+     */
+    static adjustDifficulty(lastBlock, currentTime){
+        let {difficulty} = lastBlock;
+        difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty + 1 : difficulty - 1;
+        return difficulty;
     }
 }
 
--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,6 @@
-const Block = require('./block')
+const Blockchain = require('./blockchain')
+const bc = new Blockchain();
 
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo' );
-console.log(fooBlock.toString())
+for(let i = 0; i < 10; i++){
+    console.log(bc.addBlock(`foo ${i}`).toString());
+}
